f1ce0013a1235711683b1611e1fc7995c6a36824
miksezea <mikk289e@edu.zealand.dk>
2022-09-29 14:54:40 +0200

    Task 2: Car klasse

--- TicketSystemClassLibrary/Car.cs
+++ TicketSystemClassLibrary/Car.cs
@@ -1,15 +1,48 @@
 ﻿namespace TicketSystemClassLibrary
 {
-    
+    /// <summary>
+    /// Klasse for Car
+    /// </summary>
     public class Car
     {
+        /// <summary>
+        /// Konstruktør for Car
+        /// </summary>
+        /// <param name="licenseplate">Nummerplade</param>
+        /// <param name="date">Dato</param>
         public Car(string licenseplate, DateTime date)
         {
             Licenseplate = licenseplate;
             Date = date;
         }
 
+        /// <summary>
+        /// Property for nummerplade
+        /// </summary>
         public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Property for dato
+        /// </summary>
         public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Metode til at returnere pris (double)
+        /// </summary>
+        /// <param name="price">Pris for Car</param>
+        /// <returns>240</returns>
+        public double Price(double price = 240)
+        {
+            return price;
+        }
+
+        /// <summary>
+        /// Metode til at returnere biltype (string)
+        /// </summary>
+        /// <returns>Car</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
     }
 }
