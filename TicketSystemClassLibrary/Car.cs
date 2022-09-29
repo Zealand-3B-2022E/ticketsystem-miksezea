d26556115846def2c327d8cf1460ad9d65e46623
miksezea <mikk289e@edu.zealand.dk>
2022-09-29 14:44:17 +0200

    Car Constructor

--- TicketSystemClassLibrary/Car.cs
+++ TicketSystemClassLibrary/Car.cs
@@ -1,7 +1,14 @@
 ﻿namespace TicketSystemClassLibrary
 {
+    
     public class Car
     {
+        public Car(string licenseplate, DateTime date)
+        {
+            Licenseplate = licenseplate;
+            Date = date;
+        }
+
         public string Licenseplate { get; set; }
         public DateTime Date { get; set; }
     }
