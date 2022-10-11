8afa5f279496630ad8fe2a0671fe9d633edd5899
miksezea <mikk289e@edu.zealand.dk>
2022-10-11 19:43:20 +0200

    Kommentarer tilføjet.

--- OresundTicketLibrary/model/OresundCar.cs
+++ OresundTicketLibrary/model/OresundCar.cs
@@ -2,8 +2,15 @@ using TicketSystemClassLibrary.model;
 
 namespace OresundTicketLibrary.model
 {
+    /// <summary>
+    /// Klasse for OresundCar
+    /// </summary>
     public class OresundCar : Car
     {
+        /// <summary>
+        /// Pris for Øresundsbroen. Rabat ved Brobizz = true
+        /// </summary>
+        /// <returns>pris</returns>
         public override double Price()
         {
             if (Brobizz)
@@ -16,6 +23,10 @@ namespace OresundTicketLibrary.model
             }
         }
 
+        /// <summary>
+        /// Type af køretøj
+        /// </summary>
+        /// <returns>Oresund car</returns>
         public override string VehicleType()
         {
             return "Oresund car";
--- OresundTicketLibrary/model/OresundMC.cs
+++ OresundTicketLibrary/model/OresundMC.cs
@@ -2,8 +2,15 @@ using TicketSystemClassLibrary.model;
 
 namespace OresundTicketLibrary.model
 {
+    /// <summary>
+    /// Klasse for OresundMC
+    /// </summary>
     public class OresundMC : MC
     {
+        /// <summary>
+        /// Pris for Øresundsbroen. Rabat ved Brobizz = true
+        /// </summary>
+        /// <returns>pris</returns>
         public override double Price()
         {
             if (Brobizz)
@@ -16,6 +23,10 @@ namespace OresundTicketLibrary.model
             }
         }
 
+        /// <summary>
+        /// Type af køretøj
+        /// </summary>
+        /// <returns>Oresund MC</returns>
         public override string VehicleType()
         {
             return "Oresund MC";
